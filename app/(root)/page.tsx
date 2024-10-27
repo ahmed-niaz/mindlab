4279b37e1906625706b4ce758e371854ed3c4f6f
ahmed-niaz <niazahmed.an@gmail.com>
2024-10-27 22:10:36 +0600

    complete the hero section

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,16 @@
-
-
 export default function Home() {
   return (
     <>
-      <h2 className="text-2xl">Home</h2>
+      <section className="terra_cotta_container">
+        <h2 className="heading">
+          Pitch your Startup,
+          <br />
+          Connect with Entrepreneurs
+        </h2>
+        <p className="sub-heading !max-w-3xl">
+          Submit Ideas,Vote on Pitches, and Get Noticed in Virtual Competitions
+        </p>
+      </section>
     </>
   );
 }
