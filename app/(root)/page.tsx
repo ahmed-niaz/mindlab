fa2948d4edb534095b03b34df8d10755d9257530
ahmed-niaz <niazahmed.an@gmail.com>
2024-10-30 12:29:17 +0600

    startup card and sanity

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,4 @@
+import StartupCard from "@/components/StartupCard";
 import SearchForm from "../../components/SearchForm";
 
 export default async function Home({
@@ -6,8 +7,23 @@ export default async function Home({
   searchParams: Promise<{ query?: string }>;
 }) {
   const query = (await searchParams).query;
+
+  const posts = [
+    {
+      _createdAt: new Date(),
+      views: 55,
+      author: { _id: 1,name:'niaz' },
+      _id: 1,
+      description: "this is a description",
+      image:
+        "https://images.pexels.com/photos/176842/pexels-photo-176842.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+      category: "Robots",
+      title: "Paper made",
+    },
+  ];
   return (
     <>
+      {/* hero */}
       <section className="terra_cotta_container">
         <h2 className="heading">
           Pitch your Startup,
@@ -15,9 +31,22 @@ export default async function Home({
           Connect with Entrepreneurs
         </h2>
         <p className="sub-heading !max-w-3xl">
-          Submit Ideas,Vote on Pitches, and Get Noticed in Virtual Competitions
+          Submit Ideas, Vote on Pitches, and Get Noticed in Virtual Competitions
         </p>
         <SearchForm query={query} />
+      </section>
+      {/* container */}
+      <section className="section_container">
+        <p className="text-30-semibold">
+          {query ? `Search Result for "${query}" ` : "All Startups"}
+        </p>
+        <ul className="mt-7 card_grid">
+          {posts?.length > 0 ? (
+            posts.map((post:StartupCardType) => <StartupCard key={post?._id} post={post} />)
+          ) : (
+            <p className="no-results">No startup Found</p>
+          )}
+        </ul>
       </section>
     </>
   );
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -8,7 +8,7 @@ const Navbar = async () => {
   // console.log("why", session);
 
   return (
-    <header className="px-5 py-3 bg-white shadow-sm font-work-sans">
+    <header className="px-5 py-3 bg-white shadow-sm font-work-sans max-w-[1420px] mx-auto">
       <nav className="flex justify-between items-center">
         <Link href="/">
           <Image src="/logo.png" alt="logo" width={144} height={30} />
